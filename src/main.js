797895cff36ee704b47fa3e31616c1d69d7f642c
zengjun1.fj <zengjun1.fj@ccb.com>
2024-09-30 16:30:14 +0800

    🎉 init(产物): 增加打包产物

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@
  * @Autor: zhenjun
  * @Date: 2024-09-30 10:48:28
  * @LastEditors: zhenjun
- * @LastEditTime: 2024-09-30 15:54:35
+ * @LastEditTime: 2024-09-30 16:28:20
  * @Description:
  */
 import './assets/main.css'
 
 import { createApp } from 'vue'
-
-import TDesign from 'tdesign-vue-next'
 
 import App from './App.vue'
 import router from './router'
@@ -20,7 +18,6 @@ import 'tdesign-vue-next/es/style/index.css'
 
 const app = createApp(App)
 
-app.use(TDesign)
 app.use(store)
 app.use(router)
 
--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,42 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { TDesignResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [
+        TDesignResolver({
+          library: 'vue-next'
+        })
+      ]
+    }),
+    Components({
+      resolvers: [
+        TDesignResolver({
+          library: 'vue-next'
+        })
+      ]
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
     }
   }
 })
